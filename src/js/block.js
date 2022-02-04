87aff07e4a2e87e8a47b66bb75090d40be345e82
Rodrigo Fernandes <Rodrigodatageek@gmail.com>
2022-02-04 11:55:50 +0100

    addinnd the 5 events block

---
+++ src/js/block.js
@@ -0,0 +1,23 @@
+const response = await fetch(
+    `https://test-api.codingbootcamp.cz/api/14278748/events`
+  );
+  const events = await response.json();
+  console.log(events);
+
+  events.forEach((element) => {
+    const eventItem = document.createElement("div");
+    eventItem.innerHTML = `<div class="box"><ul class= box__list>
+    </ul>
+    <li class"list__item">${element.date}</li>
+    <li class"list__item">${element.description}</li>
+    <li class"list__item">${element.id}</li>
+    <li class"list__item"><img src=${element.image_url} width="300" height="200"></li>
+    <li class"list__item">${element.name}</li>
+    </div>`;
+    document.body.appendChild(eventItem);
+  });
+
+  //   productElem.addEventListener("click", (e) => {
+  //   });
+  // });
+};
