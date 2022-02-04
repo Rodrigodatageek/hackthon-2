01b4622cd5a1b8bd41128837cf96f75a80cf5731
Daniel <virusxd521@gmail.com>
2022-02-04 12:18:01 +0200

    Daniel - Adding The First Box

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+// the main layout
+const mainDiv = document.querySelector(".main__div");
+// The URL
 const url = `https://test-api.codingbootcamp.cz/api/14278748/events`;
 
 
@@ -6,19 +9,25 @@ const myData = async () => {
   const events = await response.json();
   events.forEach((element, index) => {
       if (index === 0){
-          firstEvent(element)
+          firstBox(element)
       } else {
           console.log("Rodrigo!!!!!!!!!");
       }
   });
 };
 
-
 // Function for crearing elements 
+const creatingForm = () => {
+    const form = document.createElement("form");
+    
+}
 
 // Function for adding a form to register (Daniel's function)
-const firstEvent = data => {
+const firstBox = data => {
     console.log(data);
+    const p = document.createElement("p");
+    p.textContent = data.name;
+    mainDiv.appendChild(p);    
 }
 
 // Automatically calls the data (Daniel's funcction)
